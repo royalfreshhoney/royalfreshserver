6b6a7c359c713633b58861c9278c3f54b4821bf4
alok chaturvedi <alokc@regenesys.net>
2024-03-22 02:25:24 +0530

    db connection

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/royaladmin');
+        await mongoose.connect('mongodb+srv://royalfreshhoney:wsSb318DUOKlnvpC@cluster0.yshyqhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log("db connected successfully")
     } catch (error) {
         console.log('error connecting db')
